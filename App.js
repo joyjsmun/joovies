8367601dc99cdf1d96a7c2fec0d69a4888292673
Joy M <joy.js.mun@gmail.com>
2021-11-23 23:59:39 +0900

    ADD: assets

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native";
+import * as Font from "expo-font";
+import { Image, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
-  const startLoading = async () => {};
+  const startLoading = async () => {
+    await Asset.loadAsync(require("./sky.jpeg"));
+    await Font.loadAsync(Ionicons.font);
+    await Image.prefetch(
+      "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mbp14-spacegray-select-202110_GEO_KR?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1633657358000"
+    );
+  };
   if (!ready) {
     return (
       <AppLoading
